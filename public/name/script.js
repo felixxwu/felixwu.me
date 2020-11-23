0f44c53024da105a6c36c2a0b39a81e8870463f0
Felix Wu <felixxwu@gmail.com>
2020-11-23 20:28:02 +0000

    added /name

---
+++ public/name/script.js
@@ -0,0 +1,40 @@
+const output = document.getElementById('name')
+const google = document.getElementById('google')
+const link = document.getElementById('link')
+const historyDiv = document.getElementById('history')
+
+const getGoogleUrl = (text) => {
+    return "https://google.com/search?q=" + text.split(' ').join('+')
+}
+
+const getLinkUrl = (text) => {
+    return "https://" + text.split(' ').join('') + ".com"
+}
+
+const historyItem = (name) => {
+    const element = document.createElement('div')
+    element.className = "historyItem"
+    element.innerHTML = name
+    element.onclick = () => updateDOM(name)
+    return element
+}
+
+const updateDOM = (name) => {
+    output.innerHTML = name
+    
+    google.href = getGoogleUrl(name)
+    google.innerHTML = "Search " + name + " on Google"
+    
+    link.href = getLinkUrl(name)
+    link.innerHTML = getLinkUrl(name)
+}
+
+const randomise = () => {
+    const name = generate()
+    updateDOM(name)
+    historyDiv.insertBefore(historyItem(name), historyDiv.childNodes[0])
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    randomise()
+})
